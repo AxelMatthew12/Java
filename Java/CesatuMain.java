61ea584608119f2ed66527ffc4259bf97405292a
AxelMatthew12 <143260705+AxelMatthew12@users.noreply.github.com>
2024-04-17 15:09:46 +0700

    Project Cesatu

--- Java/CesatuMain.java
+++ Java/CesatuMain.java
@@ -4,7 +4,31 @@ import java.util.Scanner;
 public class CesatuMain {
 public static void main(String[] args) {
     Scanner sc= new Scanner(System.in);
+    
 
+    System.out.println("================ WELCOME ===================");
+    System.out.println("||            1. L O G I N                ||");
+    System.out.println("||            2. L O G O U T              ||");
+    System.out.println("============================================");
+    System.out.println("Option : ");
+    int option = sc.nextInt();
+
+    if (option == 1) {
+        System.out.println("Masukkan id anda : ");
+        String id = sc.next();
+        System.out.println("Masukkan Nama anda : ");
+        String nama = sc.next();
+        System.out.println("Password : ");
+        int password = sc.nextInt();
+        Login login = new Login(nama, id, password);
+        if (password == 123) {
+            login.Displaylogin();
+        } else {
+            System.out.println("Data atau Password anda salah ! ");
+        }
+    } else {
+        System.out.println("Anda keluar dari Program ! ");
+    }
     
     System.out.println("----------------------------------------------------");
     System.out.println("Masukkan menu anda : ");
---
+++ Java/Login.java
@@ -0,0 +1,21 @@
+package Java;
+public class Login {
+   String nama;
+   String id;
+   int password;
+
+   Login(String n , String i, int p) {
+    nama = n;
+    id = i;
+    password = p;
+   }
+
+   void Displaylogin(){
+        System.out.println("==== DATA LOGIN ===");
+        System.out.println("Nama : " + nama);
+        System.out.println("ID : " + id);
+        
+   }
+   }
+    
+
